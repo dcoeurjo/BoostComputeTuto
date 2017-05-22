f23158db53beedf7f3f74f666825614d3ebf7134
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2017-05-22 16:15:51 +0200

    apply added

---
+++ apply.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <boost/compute/core.hpp>
+#include <vector>
+
+#include <boost/compute/algorithm/transform.hpp>
+#include <boost/compute/algorithm/sort.hpp>
+#include <boost/compute/algorithm/sort.hpp>
+#include <boost/compute/container/vector.hpp>
+#include <boost/compute/functional/math.hpp>
+#include <boost/compute/algorithm/count_if.hpp>
+#include <boost/compute/function.hpp>
+
+namespace compute = boost::compute;
+
+//External function to generate integer in [0:100)
+int rand_int()
+{
+    return rand() % 100;
+}
+
+//Simple function to add 4 to an integer
+BOOST_COMPUTE_FUNCTION(int, add_four, (int x),
+{
+    return x + 4;
+});
+
+// function returing true if the point is within the unit circle
+BOOST_COMPUTE_FUNCTION(bool, is_less_than_half, (const int value),
+                       {
+                         return (value < 50);
+                       });
+
+
+int main()
+{
+
+  // get default device and setup context
+  compute::device device = compute::system::default_device();
+  compute::context context(device);
+  compute::command_queue queue(context, device);
+  
+  // generate random data on the host
+  std::vector<int> host_vector(10000); std::generate(host_vector.begin(), host_vector.end(), rand_int);
+
+  // create a vector on the device
+  compute::vector<int> device_vector(host_vector.size(), context);
+
+  // transfer data from the host to the device
+  compute::copy(host_vector.begin(), host_vector.end(), device_vector.begin(), queue);
+
+  // calculate the square-root of each element in-place
+  compute::transform(
+                     device_vector.begin(),
+                     device_vector.end(),
+                     device_vector.begin(),
+                     add_four,
+                     queue
+                     );
+  
+  
+  // count number of random int less than 50
+  size_t count = compute::count_if(device_vector.begin(),device_vector.end(), is_less_than_half, queue);
+
+  
+  std::cout << "Count= "<<count<<std::endl;  
+  
+}
